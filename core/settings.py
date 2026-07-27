f29aff1e9dacddd0dcae183ae20dd5a5abda5b49
Tomasz Brzezina <tomasz@brzezina.pl>
2026-07-27 19:50:20 +0200

    Remove Whitenoise middleware and update static file storage backend to Django's default StaticFilesStorage for improved compatibility and performance.

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -90,7 +89,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
